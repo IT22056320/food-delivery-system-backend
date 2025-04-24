2dc81f898bd44b3e3b94109dc3a9f82874e6a104
Madhini Maheshika <127973368+mdh00@users.noreply.github.com>
2025-04-25 02:21:05 +0530

    auth me api added

--- services/auth-service/src/routes/authRoutes.js
+++ services/auth-service/src/routes/authRoutes.js
@@ -3,6 +3,7 @@ const router = express.Router();
 const authCtrl = require('../controllers/authController');
 const passport = require('passport');
 const { generateToken } = require('../utils/tokenUtils');
+const { protect } = require('../middlewares/authMiddleware');
 
 router.post('/register', authCtrl.register);
 router.post('/verify-otp', authCtrl.verifyOtp);
@@ -12,23 +13,25 @@ router.post('/forgot-password', authCtrl.forgotPassword);
 router.post('/verify-reset-otp', authCtrl.verifyResetOtp);
 router.post('/reset-password', authCtrl.resetPassword);
 
+router.get('/me/', protect, authCtrl.authMe);
+
 router.get('/google',
-    passport.authenticate('google', { scope: ['profile', 'email'] })
-  );
-  
-  router.get('/google/callback',
-    passport.authenticate('google', { failureRedirect: '/login' }),
-    (req, res) => {
-      // Successful login, generate JWT & set cookie
-      const token = generateToken(req.user);
-      res.cookie('token', token, {
-        httpOnly: true,
-        sameSite: 'strict',
-        secure: process.env.NODE_ENV === 'production',
-      });
-  
-      res.redirect('/'); // Or redirect to frontend
-    }
-  );
+  passport.authenticate('google', { scope: ['profile', 'email'] })
+);
+
+router.get('/google/callback',
+  passport.authenticate('google', { failureRedirect: '/login' }),
+  (req, res) => {
+    // Successful login, generate JWT & set cookie
+    const token = generateToken(req.user);
+    res.cookie('token', token, {
+      httpOnly: true,
+      sameSite: 'strict',
+      secure: process.env.NODE_ENV === 'production',
+    });
+
+    res.redirect('/'); // Or redirect to frontend
+  }
+);
 
 module.exports = router;
